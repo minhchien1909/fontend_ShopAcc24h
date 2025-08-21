ba7a9a176685497a939d8dd80c603d39e789a215
minhthuan1809 <thuan1809minh.com>
2025-08-22 00:05:08 +0700

    refactor: remove next.config.ts and consolidate configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... các config khác nếu có
+  output: 'standalone',
   transpilePackages: ["react-quill"],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "scontent.fhan17-1.fna.fbcdn.net",
+      },
+      {
+        hostname: "res.cloudinary.com",
+        port: "", // Thêm port nếu cần thiết
+        pathname: "/**", // Thêm pathname để cho phép tất cả các đường dẫn
+      },
+      {
+        hostname: "images.unsplash.com",
+      },
+      {
+        hostname: "dev.mypagevn.com",
+      },
+      {
+        hostname: "picsum.photos",
+      },
+      {
+        hostname: "thegioihaisan.vn",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
